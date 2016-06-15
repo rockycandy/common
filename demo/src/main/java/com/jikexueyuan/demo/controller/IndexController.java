b981a1cc90a878f871e9c27e166bad17f24a51b8
Rocky.Tang <rocky_tom@163.com>
2016-06-15 17:21:51 +0800

    check-in java demo project

---
+++ demo/src/main/java/com/jikexueyuan/demo/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.jikexueyuan.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "hello")
+	public String helloWorld(){
+		return "index";
+	}
+	
+}
